3472cde838d9c4cab21797f981586be393a74f42
Muhammet Akbulut <muhammetakb68@gmail.com>
2023-08-06 13:23:17 +0300

    fix: declare global union types for transformed supabase data with ts

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { cookies } from 'next/headers';
 import AuthButtonServer from './auth-button-server';
 import { redirect } from 'next/navigation';
 import NewTweet from './new-tweet';
+import Likes from './likes';
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -15,15 +16,33 @@ export default async function Home() {
     redirect('/login');
   }
 
-  const { data: tweets } = await supabase
+  const { data } = await supabase
     .from('tweets')
-    .select('*, profiles(*)');
+    .select('*, author: profiles(*), likes(user_id)');
+
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      author: Array.isArray(tweet.author) ? tweet.author[0] : tweet.author,
+      user_has_liked_tweet: !!tweet.likes.find(
+        (like) => like.user_id === session.user.id
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
 
   return (
     <>
       <AuthButtonServer />
       <NewTweet />
-      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+      {tweets?.map((tweet) => (
+        <div key={tweet.id}>
+          <p>
+            {tweet.author.name} {tweet.author.username}
+          </p>
+          <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
+        </div>
+      ))}
     </>
   );
 }
